0dc6ca70e96b6aba0c2a6d2fd172ca1a8f45143e
Nayab Ali <nayabmca2024@gmail.com>
2026-01-30 23:35:10 +0530

    Fix contact form and update env handling

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def contact():
             msg = Message(
                 subject = f"New Contact Form Submission: {subject}",
                 sender=app.config['MAIL_USERNAME'],
-                recipients = app.config['MAIL_USERNAME'],
+                recipients = ['laraibansari1214@gmail.com'],
                 body = f"Name: {name}\nEmail: {email}\n\nSubject: {subject}\n\n\nMessage: {message_content}",
                 reply_to = email
             )
